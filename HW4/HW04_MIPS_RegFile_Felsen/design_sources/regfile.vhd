dc89c232526e8229e385446760d38a7214cacbfd
mefelsen <matthewfelsen@yahoo.com>
2019-09-30 22:37:01 -0500

    Fixed a lot of things. It's done now

--- HW4/HW04_MIPS_RegFile_Felsen/design_sources/regfile.vhd
+++ HW4/HW04_MIPS_RegFile_Felsen/design_sources/regfile.vhd
@@ -27,71 +27,44 @@ ARCHITECTURE behav OF RegFile IS
    SIGNAL RA2_index : std_logic_vector (RA2'length - 1 DOWNTO 0);
    SIGNAL WA_index  : std_logic_vector (WA'length - 1 DOWNTO 0);
    
-   component demux
-   port (
-   RegWrite    : IN std_logic;
-   sel         : IN std_logic_vector(n_bits_of(reg_file_depth) - 1 DOWNTO 0);
-   output      : OUT std_logic_vector(reg_file_width - 1 DOWNTO 0)
-   );
-   end component;
-   
-   component mux
-   PORT (
-    input : IN std_logic_vector(reg_file_width - 1 DOWNTO 0);
-    sel   : IN std_logic_vector(n_bits_of(reg_file_depth) - 1 DOWNTO 0);
-    RD    : OUT std_logic_vector(reg_file_width - 1 DOWNTO 0)
-   );
-   end component;
-   
-   component d_flipflop
-      port(
-      Q     : out std_logic;
-      en    : in std_logic;    
-      clk   : in std_logic;   
-      D     : in  std_logic;
-      rst   : in std_logic    
-   );
-   end component;
-   
-   component address_guard
-    port(
-        input     : IN std_logic_vector(n_bits_of(reg_file_depth) - 1 DOWNTO 0);
-        output    : OUT std_logic_vector(n_bits_of(reg_file_depth) - 1 DOWNTO 0)
-        );
-   end component;
-   
+  
 BEGIN
-demux1 : demux PORT MAP(
-RegWrite => RegWrite,
-sel => WA_index,
-output => reg_file
-);
 
-mux1: mux PORT MAP(
-input => RA1_index,
-sel => RA1_index,
-RD => RD1
-);
+    --WA to address guard to DEMUX, accounting for write to 0, overflow, and address spilling
+    WA_index <= WA when to_integer(unsigned(WA)) > 0 and to_integer(unsigned(WA)) < reg_file_depth
+    else (OTHERS => '0');
+    
+    --RA1 and RA2 to address guard to MUX, account for overflow, and address spilling
+    RA1_index <= RA1 when to_integer(unsigned(RA1)) >= 0 and to_integer(unsigned(RA1)) < reg_file_depth
+    else (OTHERS => '0');
+    RA2_index <= RA2 when to_integer(unsigned(RA2)) >= 0 and to_integer(unsigned(RA2)) < reg_file_depth
+    else (OTHERS => '0');
 
-mux2: mux PORT MAP(
-input => RA2_index,
-sel => RA2_index,
-RD => RD2
-);
-
-WA_guard: address_guard PORT MAP(
-input => WA,
-output => WA_index
-);
-
-RA1_guard: address_guard PORT MAP(
-input => RA1,
-output => RA1_index
-);
-
-RA2_guard: address_guard PORT MAP(
-input => RA2,
-output => RA2_index
-); 
+    --MUXs for RD1 and RD2
+    RD1 <= reg_file(to_integer(unsigned(RA1_index)));
+    RD2 <= reg_file(to_integer(unsigned(RA2_index)));
+    
+    --Process for writing to registers
+    write_access : PROCESS(clk, rst,WA,WA_index)
+    
+    BEGIN
+    --WA to address guard to DEMUX, accounting for write to 0, overflow, and address spilling
+    if to_integer(unsigned(WA)) > 0 and to_integer(unsigned(WA)) < reg_file_depth then
+        WA_index <= WA ;
+    else 
+        WA_index <= (OTHERS => '0');
+    end if;
+    if rst = '1' then
+        reg_file <= (OTHERS => zeros);
+    elsif rising_edge(clk) and RegWrite = '1' then
+      --After address guarding, WD can write to index
+      if to_integer(unsigned(WA_index)) /= 0 then
+        reg_file(to_integer(unsigned(WA_INDEX))) <= WD;
+      end if;
+    end if;       
+    
+    END PROCESS write_access;
+    
+    
 
 END behav;
